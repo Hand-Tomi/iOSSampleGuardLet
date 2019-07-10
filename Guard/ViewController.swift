4e9df8595b897a5cbd0bd693645bbad41fd89838
SonSeMyeong <seaud5@gmail.com>
2019-07-11 00:42:57 +0900

    guard let Sample적용

--- Guard/ViewController.swift
+++ Guard/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alertLabel: UILabel!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        alertLabel.text = ""
+        alertLabel.textColor = UIColor.red
     }
 
-
+    @IBAction func doneLogin(_ sender: Any) {
+        
+        guard let username = usernameField.text, username.count > 0 else {
+            // 없는 경우 추가 작업을 할 수 있다.
+            alertLabel.text = "username is Empty"
+            return // return이 반드시 있어야한다.
+        }
+        
+        guard let password = passwordField.text, password.count > 0 else {
+            alertLabel.text = "password is Empty"
+            return
+        }
+        
+        if username == "kim" && password == "1234" {
+            alertLabel.text = "login Success"
+        } else {
+            alertLabel.text = "login Failed"
+        }
+        
+    }
 }
 
